eab45cb44bd1569f571874394ed481d31c951370
windfly <974589985@qq.com>
2021-03-05 18:17:12 +0800

    注释无用的代码

--- pages/api/favicon.js
+++ pages/api/favicon.js
@@ -1,15 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const url = require('url')
-const request = require('request')
+// const url = require('url')
+// const request = require('request')
 export default (req, res) => {
-  const { page_url, size = 24 } = req.query
-  // 根据page_url,size返回特定大小的的网址图片
-  const urlObj = url.parse(page_url)
-  if(urlObj.slashes && urlObj.protocol && urlObj.host) {
-    const faviconIconUrl = `${urlObj.protocol}//${urlObj.host}/facicon.ico`
-    request.get('http://google.com/img.png').pipe(res)
-  } else {
-    // 创建一个图片流返回
-  }
+  // const { page_url, size = 24 } = req.query
+  // // 根据page_url,size返回特定大小的的网址图片
+  // const urlObj = url.parse(page_url)
+  // if(urlObj.slashes && urlObj.protocol && urlObj.host) {
+  //   const faviconIconUrl = `${urlObj.protocol}//${urlObj.host}/facicon.ico`
+  //   request.get('http://google.com/img.png').pipe(res)
+  // } else {
+  //   // 创建一个图片流返回
+  // }
   res.status(200).json({ name: 'John Doe' })
 }
